ffe77becc85c8a3830f5aba5e8af4cd388ea9e26
s4viom3ndes <savioAlexandre.22@hotmail.com>
2023-11-19 15:14:37 -0300

    first commit

---
+++ Backend/src/models/tasksModel.js
@@ -0,0 +1,13 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+    const tasks = await connection.execute('SELECT * FROM tasks');
+    return tasks;
+};
+
+
+
+
+module.exports = {
+    getAll
+};
